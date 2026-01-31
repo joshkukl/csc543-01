08b3db31fb2d18a2b85330e55e75eb214d3d1826
user <kuklinskij1@southernct.edu>
2026-01-31 17:50:46 -0500

    Changes made to intro.tsx for assignment 2

---
+++ components/intro.tsx
@@ -0,0 +1,11 @@
+export default function Intro() {
+  return (
+    <div>
+        <h1>Intro</h1>
+        <p>Paragraph for assignment 2.</p>
+        <ul>
+            <li>Here is a list of one item.</li>
+        </ul>
+    </div>
+  );
+}
